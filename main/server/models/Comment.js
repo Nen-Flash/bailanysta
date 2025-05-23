4d26654503367c01b6e51b1d9b0d4e48cf716344
Nen-Flash <166835490+Nen-Flash@users.noreply.github.com>
2025-05-24 00:44:27 +0500

    Delete main directory

--- main/server/models/Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-  postId: String,
-  author: String,
-  content: String,
-});
-
-module.exports = mongoose.model('Comment', commentSchema);
